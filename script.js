f40b184a3e5450a108d8bb8beb5f2ba1d6988c56
Jordan Roenitz <61602629+jroenitz@users.noreply.github.com>
2020-03-20 11:28:03 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+//Has all leters, numbers and special charaters as well as how many letters 
+function generatePassword() {
+  var length = 10,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()",
+      retVal = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+      retVal += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return retVal;
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
